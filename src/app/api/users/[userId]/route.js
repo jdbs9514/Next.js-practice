244987f0c0c2ad0d60fb2a1472699c14ca1b5e36
Josue Benavides <jdbs9514@gmail.com>
2024-02-14 17:08:18 -0500

    queries params

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -2,6 +2,14 @@ import { NextResponse } from "next/server"
 //porcess params from the URL
 //when you use a dynamic route, you can use the params object to access the dynamic route parameters
 export const GET = async (request, { params }) => {
+
+  //query parameters
+  const { searchParams } = new URL(request.url);
+  console.log(searchParams);
+  console.log(searchParams.get('name'));
+  console.log(searchParams.get('email'));
+  //
+
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`)
   const data = await res.json()
   return NextResponse.json({ 
